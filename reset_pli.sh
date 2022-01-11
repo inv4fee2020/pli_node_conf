ffb4a371109cf6c75e89916174e6f565cf0ad8b7
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-11 13:03:28 +0000

    minor update

--- reset_pli.sh
+++ reset_pli.sh
@@ -2,23 +2,22 @@
 
 ##  Rough script to roll back installation for testing purposes...
 ## Use with caution !
-sudo su
+#sudo su
 sudo pm2 stop all && sudo pm2 delete all
 
 sudo systemctl status postgresql && sudo systemctl stop postgresql
 
-rm -rf /usr/local/go
-rm -rf /pli_node/plugin-deployment/Plugin/
-rm -rf /usr/lib/postgresql/ && rm -rf /var/lib/postgresql/ && rm -rf /var/log/postgresql/ && rm -rf /etc/postgresql/ && rm -rf /etc/postgresql-common/
+sudo rm -rf /usr/local/go
+sudo rm -rf /pli_node/plugin-deployment
+sudo rm -rf /usr/lib/postgresql/ && sudo rm -rf /var/lib/postgresql/ && sudo rm -rf /var/log/postgresql/ && sudo rm -rf /etc/postgresql/ && rsudo m -rf /etc/postgresql-common/
 
 
-apt --purge remove postgresql* -y && apt purge postgresql* -y 
-apt --purge remove postgresql postgresql-doc postgresql-common -y
-apt autoremove
+sudo apt --purge remove postgresql* -y && sudo apt purge postgresql* -y 
+sudo apt --purge remove -y postgresql postgresql-doc postgresql-common -y
+sudo apt autoremove
 
-rm -rf /usr/lib/postgresql && rm -rf /var/lib/postgresql && rm -rf /var/log/postgresql && rm -rf /etc/postgresql && rm -rf /etc/postgresql-common
+sudo rm -rf /usr/lib/postgresql/ && sudo rm -rf /var/lib/postgresql/ && sudo rm -rf /var/log/postgresql/ && sudo rm -rf /etc/postgresql/ && rsudo m -rf /etc/postgresql-common/
 
-userdel -r postgres && groupdel postgres
+sudo userdel -r postgres && sudo groupdel postgres
 
-cd /root
-rm -rf .cache/ && rm -rf .nvm && rm -rf .npm && rm -rf .plugin && rm -rf .pm2 && rm -rf work/
+sudo su -c "cd /root; rm -rf .cache/ && rm -rf .nvm && rm -rf .npm && rm -rf .plugin && sudo rm -rf .pm2 && rm -rf work/"
