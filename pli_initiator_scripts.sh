cec6d21fc34b700c3cf3647aec8a0c0f0afff87e
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-12 09:53:53 +0000

    corrected PATH var PLI_DEPLOY

--- pli_initiator_scripts.sh
+++ pli_initiator_scripts.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     PLI_BASE_DIR="pli_node"
     PLI_DEPLOY_DIR="plugin-deployment"
-    PLI_DEPLOY_PATH="/$PLI_BASE_DIR/$PLI_DEPLOY/"
+    PLI_DEPLOY_PATH="/$PLI_BASE_DIR/$PLI_DEPLOY_DIR/"
     
     PLI_INITOR_DIR="external-Initiator"
     PLI_L_INIT_NAME="xdc"
@@ -18,6 +18,7 @@ NC='\033[0m' # No Color
     PLI_INIT_RAWFILE="pli_init.raw"
     PLI_INIT_DATFILE="pli_init.dat"
     BASH_FILE3="3_InitiatorStartPM2.sh"
+
     PLI_HTTP_PORT="6688"
     PLI_HTTPS_PORT="6689"
 
@@ -47,75 +48,36 @@ FUNC_INITIATOR(){
     echo 
     export FEATURE_EXTERNAL_INITIATORS=true
     plugin admin login
-    plugin initiators create $PLI_L_INIT_NAME http://localhost:8080/jobs > pli_init.raw
+    plugin initiators create $PLI_L_INIT_NAME http://localhost:8080/jobs > $PLI_INIT_RAWFILE
 
-plugin initiators create xdc http://localhost:8080/jobs
-plugin initiators destroy xdc http://localhost:8080/jobs
-plugin initiators create xdc http://localhost:8080/jobs > pli_init.raw
-
-
-sed -i 's/ ║ /,/g;s/╬//g;s/═//g;s/║//g' pli_init.raw
-sed -n '/xdc/,//p' pli_init.raw > pli_init.dat
-sed -i 's/,/\n/g;s/^.xdc//g' pli_init.dat
-sed -i 's/^http.*//g' pli_init.dat
-sed -i.bak '/^$/d;/^\s*$/d;s/[ \t]\+$//' pli_init.dat
-cat pli_init.dat
+    plugin initiators create xdc http://localhost:8080/jobs
+    plugin initiators destroy xdc http://localhost:8080/jobs
+    plugin initiators create xdc http://localhost:8080/jobs > $PLI_INIT_RAWFILE
 
 
-read -r -d '' EXT_ACCESSKEY EXT_SECRET EXT_OUTGOINGTOKEN EXT_OUTGOINGSECRET <pli_init.dat
+sed -i 's/ ║ /,/g;s/╬//g;s/═//g;s/║//g' $PLI_INIT_RAWFILE
+sed -n '/xdc/,//p' $PLI_INIT_RAWFILE > $PLI_INIT_DATFILE
+sed -i 's/,/\n/g;s/^.xdc//g' $PLI_INIT_DATFILE
+sed -i 's/^http.*//g' $PLI_INIT_DATFILE
+sed -i.bak '/^$/d;/^\s*$/d;s/[ \t]\+$//' $PLI_INIT_DATFILE
+cat $PLI_INIT_DATFILE
+sleep 1s
+
+
+read -r -d '' EXT_ACCESSKEY EXT_SECRET EXT_OUTGOINGTOKEN EXT_OUTGOINGSECRET <$PLI_INIT_DATFILE
 echo
 echo "$EXT_ACCESSKEY"
 echo "$EXT_SECRET"
 echo "$EXT_OUTGOINGTOKEN"
 echo "$EXT_OUTGOINGSECRET"
-
-
-    cat <<EOF >> $BASH_FILE3
-#!/bin/bash
-export EI_DATABASEURL=postgresql://postgres:${DB_PWD_REPLACE}@127.0.0.1:5432/plugin_mainnet_db?sslmode=disable
-export EI_CHAINLINKURL=http://localhost:6688
-export EI_IC_ACCESSKEY=${EXT_ACCESSKEY}
-export EI_IC_SECRET=${EXT_SECRET}
-export EI_CI_ACCESSKEY=${EXT_OUTGOINGTOKEN}
-export EI_CI_SECRET=${EXT_OUTGOINGSECRET}
-echo *** Starting EXTERNAL INITIATOR ***
-external-initiator "{\"name\":\"$PLI_E_INIT_NAME\",\"type\":\"xinfin\",\"url\":\"https://pluginrpc.blocksscan.io\"}" --chainlinkurl "http://localhost:6688/"
-EOF
-
-
-
-
-
-
-
-sed -i 's/ ║ /,/g' pli_init.raw
-sed -i 's/╬//g;s/═//g;s/║//g' pli_init.raw
-sed -i 's/╬//g' pli_init.raw
-sed -i 's/═//g' pli_init.raw
-sed -i 's/║//g' pli_init.raw
-sed -n '/xdc/,//p' pli_init.raw > pli_init.dat
-sed -i 's/,/\n/g;s/^.xdc//g;s/^http.*//g' pli_init.dat
-sed -i 's/,/\n/g' pli_init.dat
-sed -i 's/^.xdc//g' pli_init.dat
-sed -i.bak 's/^http.*//g' pli_init.dat
-sed -i.bak '/^$/d;/^\s*$/d;s/[ \t]\+$//' pli_init.dat
-sed -i '/^$/d' pli_init.dat
-sed -i '/^\s*$/d' pli_init.dat
-sed -i 's/[ \t]\+$//' pli_init.dat
-
-
-read -r -d '' EXT_ACCESSKEY EXT_SECRET EXT_OUTGOINGTOKEN EXT_OUTGOINGSECRET <init.strip
-echo
-echo "$EXT_ACCESSKEY"
-echo "$EXT_SECRET"
-echo "$EXT_OUTGOINGTOKEN"
-echo "$EXT_OUTGOINGSECRET"
+sleep 1s
 
 
     echo -e "${GREEN}#########################################################################"
     echo
     echo -e "${GREEN}## CREATE INITIATOR PM2 SERVICE FILE: $BASH_FILE3 & file perms ${NC}"
     echo
+    cd $PLI_DEPLOY_PATH
     cat <<EOF >> $BASH_FILE3
 #!/bin/bash
 export EI_DATABASEURL=postgresql://postgres:${DB_PWD_REPLACE}@127.0.0.1:5432/plugin_mainnet_db?sslmode=disable
@@ -127,7 +89,7 @@ export EI_CI_SECRET=${EXT_OUTGOINGSECRET}
 echo *** Starting EXTERNAL INITIATOR ***
 external-initiator "{\"name\":\"$PLI_E_INIT_NAME\",\"type\":\"xinfin\",\"url\":\"https://pluginrpc.blocksscan.io\"}" --chainlinkurl "http://localhost:6688/"
 EOF
-
+    sleep 1s
     cat $BASH_FILE3
     chmod u+x $BASH_FILE3
 
@@ -137,6 +99,8 @@ EOF
     echo -e "${GREEN}## START INITIATOR PM2 SERVICE $BASH_FILE3 ${NC}"
     echo    
     pm2 start $BASH_FILE3
+    sleep 2s
+    pm2 status
 
 }
 
