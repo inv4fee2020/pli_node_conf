ad82581ad3e71ef35a3c32b7a64efcf7d4a4dd33
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-12 10:16:44 +0000

    testing completed good

--- pli_initiator_scripts.sh
+++ pli_initiator_scripts.sh
@@ -19,6 +19,9 @@ NC='\033[0m' # No Color
     PLI_INIT_DATFILE="pli_init.dat"
     BASH_FILE3="3_InitiatorStartPM2.sh"
 
+
+    DB_PWD_REPLACE="testdbpwd1234"
+
     PLI_HTTP_PORT="6688"
     PLI_HTTPS_PORT="6689"
 
@@ -26,9 +29,12 @@ NC='\033[0m' # No Color
 FUNC_INITIATOR(){
 
 
-    echo -e "${GREEN}############# - UNTESTED BETA - UNTESTED BETA - UNTESTED BETA ############################################################"
+    
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
     echo
-    echo -e "${GREEN}## INSTALL LOCAL INITIATOR...${NC}"
+    echo -e "${GREEN}## CLONE & INSTALL LOCAL INITIATOR...${NC}"
     echo 
 
     # Added to resolve error running 'plugin help'
@@ -42,7 +48,9 @@ FUNC_INITIATOR(){
 
 
 
-    echo -e "${GREEN}#########################################################################"
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
     echo
     echo -e "${GREEN}## CREATE LOCAL INITIATOR...${NC}"
     echo 
@@ -50,35 +58,49 @@ FUNC_INITIATOR(){
     plugin admin login
     plugin initiators create $PLI_L_INIT_NAME http://localhost:8080/jobs > $PLI_INIT_RAWFILE
 
-    plugin initiators create xdc http://localhost:8080/jobs
-    plugin initiators destroy xdc http://localhost:8080/jobs
-    plugin initiators create xdc http://localhost:8080/jobs > $PLI_INIT_RAWFILE
+    # plugin initiators create xdc http://localhost:8080/jobs
+    # plugin initiators destroy xdc http://localhost:8080/jobs
+    # plugin initiators create xdc http://localhost:8080/jobs > $PLI_INIT_RAWFILE
 
 
-sed -i 's/ ║ /,/g;s/╬//g;s/═//g;s/║//g' $PLI_INIT_RAWFILE
-sed -n '/xdc/,//p' $PLI_INIT_RAWFILE > $PLI_INIT_DATFILE
-sed -i 's/,/\n/g;s/^.xdc//g' $PLI_INIT_DATFILE
-sed -i 's/^http.*//g' $PLI_INIT_DATFILE
-sed -i.bak '/^$/d;/^\s*$/d;s/[ \t]\+$//' $PLI_INIT_DATFILE
-cat $PLI_INIT_DATFILE
-sleep 1s
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## CAPTURE INITIATOR CREDENTIALS & FILE MANIPULATION...${NC}"
+    echo 
+    sed -i 's/ ║ /,/g;s/╬//g;s/═//g;s/║//g' $PLI_INIT_RAWFILE
+    sed -n '/xdc/,//p' $PLI_INIT_RAWFILE > $PLI_INIT_DATFILE
+    sed -i 's/,/\n/g;s/^.xdc//g' $PLI_INIT_DATFILE
+    sed -i 's/^http.*//g' $PLI_INIT_DATFILE
+    sed -i.bak '/^$/d;/^\s*$/d;s/[ \t]\+$//' $PLI_INIT_DATFILE
+    cat $PLI_INIT_DATFILE
+    sleep 1s
 
 
-read -r -d '' EXT_ACCESSKEY EXT_SECRET EXT_OUTGOINGTOKEN EXT_OUTGOINGSECRET <$PLI_INIT_DATFILE
-echo
-echo "$EXT_ACCESSKEY"
-echo "$EXT_SECRET"
-echo "$EXT_OUTGOINGTOKEN"
-echo "$EXT_OUTGOINGSECRET"
-sleep 1s
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## READ INITIATOR CREDENTIALS AS VARIABLES...${NC}"
+    echo 
+    read -r -d '' EXT_ACCESSKEY EXT_SECRET EXT_OUTGOINGTOKEN EXT_OUTGOINGSECRET <$PLI_INIT_DATFILE
+    echo
+    #echo "$EXT_ACCESSKEY"
+    #echo "$EXT_SECRET"
+    #echo "$EXT_OUTGOINGTOKEN"
+    #echo "$EXT_OUTGOINGSECRET"
+    sleep 1s
 
 
-    echo -e "${GREEN}#########################################################################"
+    echo
+    echo
+    echo -e "${GREEN}#########################################################################${NC}"
     echo
     echo -e "${GREEN}## CREATE INITIATOR PM2 SERVICE FILE: $BASH_FILE3 & file perms ${NC}"
     echo
     cd $PLI_DEPLOY_PATH
-    cat <<EOF >> $BASH_FILE3
+    cat <<EOF > $BASH_FILE3
 #!/bin/bash
 export EI_DATABASEURL=postgresql://postgres:${DB_PWD_REPLACE}@127.0.0.1:5432/plugin_mainnet_db?sslmode=disable
 export EI_CHAINLINKURL=http://localhost:6688
@@ -94,7 +116,9 @@ EOF
     chmod u+x $BASH_FILE3
 
 
-    echo -e "${GREEN}#########################################################################"
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
     echo
     echo -e "${GREEN}## START INITIATOR PM2 SERVICE $BASH_FILE3 ${NC}"
     echo    
