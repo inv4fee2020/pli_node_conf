5d3be699496a97a911153627836e400c7eaec9ba
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-20 00:24:10 +0000

    testing db backup scripts

---
+++ backup_db.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+source ~/"plinode_$(hostname -f)".vars
+
+FUNC_DB_VARS(){
+## VARIABLE / PARAMETER DEFINITIONS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get current user id and store as var
+    USER_ID=$(getent passwd $EUID | cut -d: -f1)
+
+
+    PLI_DB_VARS_FILE=~/"plinode_$(hostname -f)"_sql.vars
+    if [ ! -e ~/$PLI_DB_VARS_FILE ]; then
+        clear
+        echo
+        echo
+        echo -e "${RED} #### ERROR: No VARIABLES file found. ####${NC}"
+        echo
+        echo -e "${RED} ..creating local vars file '$HOME/$PLI_DB_VARS_FILE' ${NC}"
+        cp sample_sql.vars ~/$PLI_DB_VARS_FILE
+        chmod 600 ~/$PLI_DB_VARS_FILE
+        echo
+        echo -e "${GREEN} please update the vars file with your specific values.. ${NC}"
+        echo -e "${GREEN} copy command to edit: ${NC}"
+        echo
+        echo -e "${GREEN}nano ~/$PLI_VARS_FILE ${NC}"
+        echo
+        echo
+        #sleep 2s
+        exit 1
+    fi
+    source ~/$PLI_DB_VARS_FILE
+
+}
+
+FUNC_DB_VARS;
+sleep 1s
+
+sudo su postgres -c "export PGPASSFILE="/$DB_BACKUP_PATH/.pgpass"; pg_dump -c -w -U postgres plugin_mainnet_db | gzip > /"$DB_BACKUP_ROOT"/"$DB_BACKUP_FNAME".sql.gz"
+sudo chown $DB_BACKUP_FUSER:$DB_BACKUP_GUSER /"$DB_BACKUP_DIR"/"$DB_BACKUP_FNAME".sql.gz
+sleep 0.5s
+gpg --yes --batch --passphrase=${PASS_KEYSTORE} -o /$ENC_PATH/$ENC_FNAME -c /${DB_BACKUP_ROOT}/${DB_BACKUP_FNAME}.sql.gz
+sudo chown $DB_BACKUP_FUSER:$DB_BACKUP_GUSER /$ENC_PATH/$ENC_FNAME
+
+
+
