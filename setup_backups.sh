afa398b2c20385f3ba0c054a2c8fe42af4b60a4f
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-22 14:29:18 +0000

    build out setup for backups

---
+++ setup_backups.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Authenticate sudo perms before script execution to avoid timeouts or errors
+sudo -l > /dev/null 2>&1
+
+# create folders
+
+
+# add new users (array)
+
+
+# assign permissions for users/folders
+
+
+# install gupload & sed to conf file
+
+
+# where to store the backup script???
+
+# create the cron job & restart cron
