776a1a2469901f8f441f06aa730edc44ab410409
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-23 11:57:57 +0000

    db script dev

--- db_run_backup.sh
+++ db_run_backup.sh
@@ -3,13 +3,16 @@
 # Authenticate sudo perms before script execution to avoid timeouts or errors
 sudo -l > /dev/null 2>&1
 
+
+# Get current user id and store as var
+USER_ID=$(getent passwd $EUID | cut -d: -f1)
+
 source ~/"plinode_$(hostname -f)".vars
+
 
 FUNC_DB_VARS(){
 ## VARIABLE / PARAMETER DEFINITIONS
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Get current user id and store as var
-    USER_ID=$(getent passwd $EUID | cut -d: -f1)
 
 
     PLI_DB_VARS_FILE="plinode_$(hostname -f)"_sql.vars
@@ -33,18 +36,40 @@ FUNC_DB_VARS(){
         exit 1
     fi
     source ~/$PLI_DB_VARS_FILE
-
 }
 
-FUNC_DB_VARS;
 sleep 1s
 
-sudo su postgres -c "export PGPASSFILE="/$DB_BACKUP_PATH/.pgpass"; pg_dump -c -w -U postgres plugin_mainnet_db | gzip > /$DB_BACKUP_OBJ"
+
+FUNC_DB_BACKUP_LOCAL(){
+sudo usermod -aG postgres $(getent passwd $EUID | cut -d: -f1)
+
+cat <<EOF >> .pgpass
+Localhost:5432:$DB_NAME:postgres:$DB_PWD_NEW
+EOF
+chmod 600 ~/.pgpass
+cp ~/.pgpass /$DB_BACKUP_PATH/.pgpass
+sudo chown postgres:postgres /$DB_BACKUP_PATH/.pgpass
+
+
+sudo su postgres -c "export PGPASSFILE="/$DB_BACKUP_PATH/.pgpass"; pg_dump -c -w -U postgres $DB_NAME | gzip > /$DB_BACKUP_OBJ"
 sudo chown $DB_BACKUP_FUSER:$DB_BACKUP_GUSER /$DB_BACKUP_OBJ
 sleep 0.5s
 
 gpg --yes --batch --passphrase=$PASS_KEYSTORE -o /$ENC_PATH/$ENC_FNAME -c /$DB_BACKUP_OBJ
 sudo chown $DB_BACKUP_FUSER:$DB_BACKUP_GUSER /$ENC_PATH/$ENC_FNAME
 #rm -f /$DB_BACKUP_OBJ
+}
+
+
+
+FUNC_DB_BACKUP_REMOTE(){
+# add check that gupload is installed!
 sudo su gdbackup -c "cd ~/; .google-drive-upload/bin/gupload -q -d /$DB_BACKUP_PATH/*.gpg -C $(hostname -f) --hide"
 
+}
+
+
+FUNC_DB_VARS;
+FUNC_DB_BACKUP_LOCAL;
+FUNC_DB_BACKUP_REMOTE;
