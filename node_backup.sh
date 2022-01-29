e1136d841d8f336f1bee427b0fe73060834e8d52
inv4fee2020 <inv4fee2020@gmail.com>
2022-01-29 23:05:03 +0000

    backup folder perms - sql failing no perms

--- node_backup.sh
+++ node_backup.sh
@@ -147,7 +147,8 @@ fi
         #echo "sudo chown $USER_ID:$DB_BACKUP_GUSER -R "/$DB_BACKUP_ROOT/$DB_BACKUP_DIR""
         sudo chown $USER_ID:$DB_BACKUP_GUSER -R "/$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
         #echo "sudo chmod g+w -R "/$DB_BACKUP_ROOT/$DB_BACKUP_DIR""
-        #sudo chmod g+r -R "/$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
+        sudo chmod g+rw -R "/$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
+        sudo chmod o-rx -R "/$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
         # Updates the 'DB_BACKUP_PATH' & 'DB_BACKUP_OBJ' variable
         DB_BACKUP_PATH="/$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
         echo "checking vars - assigning 'DB_BACKUP_PATH' variable: "$DB_BACKUP_PATH""
@@ -159,7 +160,8 @@ fi
         #echo "sudo chown $USER_ID:$DB_BACKUP_GUSER -R "$DB_BACKUP_ROOT/$DB_BACKUP_DIR""
         sudo chown $USER_ID:$DB_BACKUP_GUSER -R "$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
         #echo "sudo chmod g+w -R "$DB_BACKUP_ROOT/$DB_BACKUP_DIR""
-        #sudo chmod g+r -R "$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
+        sudo chmod g+rw -R "$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
+        sudo chmod o-rx -R "$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
         # Updates the 'DB_BACKUP_PATH' & 'DB_BACKUP_OBJ' variable
         DB_BACKUP_PATH="$DB_BACKUP_ROOT/$DB_BACKUP_DIR"
         echo "checking vars - assigning 'DB_BACKUP_PATH' variable: "$DB_BACKUP_PATH""
