7ddf8357bd38e1ba910fd132aba5a7c179ace9e0
inv4fee2020 <inv4fee2020@gmail.com>
2022-03-23 00:08:11 +0000

    api pass

--- gen_passwd.sh
+++ gen_passwd.sh
@@ -2,6 +2,7 @@
 
 PASS_KEYSTORE=""
 PASS_POSTGRES=""
+PASS_API=""
 BEGIN=true
 AGAIN=false
 
@@ -43,7 +44,7 @@ upper='[[:upper:]].*[[:upper:]].*[[:upper:]]'
 lower='[[:lower:]].*[[:lower:]].*[[:lower:]]'
 
 while $BEGIN || (( len_pass < 8 && len_pass > 28 )) ||
-    [[ ! ( $test_pass =~ $digits && $test_pass =~ $upper && $test_pass =~ $lower && $test_pass =~ $punct ) ]]
+    [[ ! ( $test_pass =~ $digits && $test_pass =~ $upper && $test_pass =~ $lower ) ]]
 do
     #$AGAIN && echo "###  Warning: Does not meet complexity - rehashing!" || AGAIN=true
     $AGAIN || AGAIN=true
@@ -64,6 +65,34 @@ echo "$PASS_POSTGRES"
 
 }
 
+FUNC_GEN_API(){
+    
+digits='[[:digit:]].*[[:digit:]].*[[:digit:]]'
+upper='[[:upper:]].*[[:upper:]].*[[:upper:]]'
+lower='[[:lower:]].*[[:lower:]].*[[:lower:]]'
+
+while $BEGIN || (( len_pass < 8 && len_pass > 13 )) ||
+    [[ ! ( $test_pass =~ $digits && $test_pass =~ $upper && $test_pass =~ $lower ) ]]
+do
+    #$AGAIN && echo "###  Warning: Does not meet complexity - rehashing!" || AGAIN=true
+    $AGAIN || AGAIN=true
+    BEGIN=false
+    PASS_API=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
+    if [[ ! -z $PASS_API ]]
+    then
+        len_pass=${#PASS_API}
+        test_pass=${PASS_API#?}
+        test_pass=${test_pass%?}
+    else
+        BEGIN=true
+    fi
+done
+
+#echo "new password is $PASS_API"
+echo "$PASS_API"
+
+}
+
 
 case "$1" in
         -keys)
@@ -71,6 +100,9 @@ case "$1" in
                 ;;
         -db)
                 FUNC_GEN_POSTGRES
+                ;;
+        -api)
+                FUNC_GEN_API
                 ;;
         *)
                 echo 
@@ -83,5 +115,6 @@ case "$1" in
                 echo 
                 echo "      -db      ==  Randomly generates new Postgres compatible password as defined in readme & VARS file"
                 echo 
+                echo "      -api     ==  Randomly generates new node gui compatible password as defined in readme & VARS file"
                 echo 
 esac
