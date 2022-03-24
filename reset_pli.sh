43c38012ae27b0874c87ec16cb73ecd4572db033
inv4fee2020 <inv4fee2020@gmail.com>
2022-03-24 21:30:49 +0000

    warning & confirmation added

--- reset_pli.sh
+++ reset_pli.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+# Set Colour Vars
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+    echo -e "${RED}#########################################################################"
+    echo -e "${RED}#########################################################################"
+    echo -e "${RED}"
+    echo -e "${NC}                   !! Plugin Node Reset Script !!${NC}"
+    echo -e "${RED}"
+    echo -e "${RED}#########################################################################"
+    echo -e "${RED}#########################################################################${NC}"
+    echo
+    echo
+    echo
+
+
+
+    # Ask the user acc for login details (comment out to disable)
+    CHECK_PASSWD=false
+        while true; do
+            read -t10 -r -p "${RED}:: WARNING ::${NC}Confirm that you wish to RESET your Plugin node installation ? (Y/n) " _input
+            if [ $? -gt 128 ]; then
+                #clear
+                echo
+                echo "timed out waiting for user response - quitting..."
+                exit 0
+            fi
+            case $_input in
+                [Yy][Ee][Ss]|[Yy]* )
+                    break
+                    ;;
+                [Nn][Oo]|[Nn]* ) 
+                    exit 0
+                    ;;
+                * ) echo "Please answer (y)es or (n)o.";;
+            esac
+        done
+
+
 # Authenticate sudo perms before script execution to avoid timeouts or errors
 sudo -l > /dev/null 2>&1
 
@@ -20,7 +60,7 @@ sudo systemctl stop postgresql
 # Delete folders for; Go install, plugin-deployment install, POSTGRES.
 sudo rm -rf /usr/local/go
 sudo rm -rf /$PLI_DEPLOY_PATH
-sudo rm -rf /usr/lib/postgresql/ && sudo rm -rf /var/lib/postgresql/ && sudo rm -rf /var/log/postgresql/ && sudo rm -rf /etc/postgresql/ && sudo m -rf /etc/postgresql-common/
+sudo rm -rf /usr/lib/postgresql/ && sudo rm -rf /var/lib/postgresql/ && sudo rm -rf /var/log/postgresql/ && sudo rm -rf /etc/postgresql/ && sudo rm -rf /etc/postgresql-common/
 
 # Remove the POSTGRES packages & clean up linked packages
 sudo apt --purge remove postgresql* -y && sudo apt purge postgresql* -y 
@@ -35,5 +75,10 @@ sudo userdel -r postgres && sudo groupdel postgres
 
 # Remove all plugin, nodejs linked folders for current user & root
 cd ~/; sudo sh -c "rm -rf .cache/ && rm -rf .nvm && rm -rf .npm && rm -rf .plugin && rm -rf Plugin && rm -rf .pm2 && rm -rf work && rm -rf go && rm -rf .yarn*"
-#sleep 0.5s
-#sudo su -c "cd /root; rm -rf .cache/ && rm -rf .nvm && rm -rf .npm && rm -rf .plugin && rm -rf .pm2 && rm -rf work && rm -rf go && rm -rf .yarn*"
+
+echo -e "${NC} Be sure to manually update your '.profile' file for remaining variables..."
+
+
+FUNC_EXIT(){
+	exit 0
+	}
