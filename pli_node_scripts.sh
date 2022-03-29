d0e7c91b98bc2a4160897d79f0166ef56b3ec27f
inv4fee2020 <inv4fee2020@gmail.com>
2022-03-29 01:46:08 +0100

    source profile bug

--- pli_node_scripts.sh
+++ pli_node_scripts.sh
@@ -579,11 +579,11 @@ EOF
 FUNC_EXIT(){
     # remove the sudo timeout for USER_ID
     sudo sh -c 'rm -f /etc/sudoers.d/plinode_deploy'
-    source ~/.profile;
-    #export GOROOT=/usr/local/go
-    #export GOPATH=$HOME/work
-    #export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-    #export FEATURE_EXTERNAL_INITIATORS=true
+    #source ~/.profile;
+    export GOROOT=/usr/local/go
+    export GOPATH=$HOME/work
+    export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+    export FEATURE_EXTERNAL_INITIATORS=true
     #source ~/.profile
 	exit 0
 	}
