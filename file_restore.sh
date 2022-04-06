6908b93f9b3600185d9c93154b482692d093f0ed
inv4fee2020 <inv4fee2020@gmail.com>
2022-04-07 00:01:31 +0100

    restore

--- file_restore.sh
+++ file_restore.sh
@@ -54,7 +54,8 @@ FUNC_RESTORE_DB(){
 
 
     echo "   DB RESTORE.... psql file name: $RESTORE_FILE_SQL"
-    sudo su postgres -c "export PGPASSFILE="$DB_BACKUP_PATH/.pgpass"; pg_restore -U username -f $DB_NAME -f $RESTORE_FILE_SQL"
+    sudo su postgres -c "export PGPASSFILE="$DB_BACKUP_PATH/.pgpass"; pg_restore -d $DB_NAME -f $RESTORE_FILE_SQL"
+    #sudo su postgres -c "export PGPASSFILE="$DB_BACKUP_PATH/.pgpass"; pg_restore -U ß -f $DB_NAME -f $RESTORE_FILE_SQL"
     #sudo su postgres -c "export PGPASSFILE="$DB_BACKUP_PATH/.pgpass"; psql -U username -d $DB_NAME < $RESTORE_FILE_SQL > /dev/null 2>&1"
     # this fails as sudo home path is taken... required node_backups folder in / to reduce complexity
     #sudo su postgres -c "export PGPASSFILE="/home/$USER_ID/node_backups/.pgpass"; gunzip -c /home/$USER_ID/node_backups/racknerd-ac9ce7_plugin_mainnet_db_2022_04_03_23_06.sql.gz | psql -U postgres -d plugin_mainnet_db  > /dev/null 2>&1"
