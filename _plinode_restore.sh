89273f01e61355ab55ab42e70415a04b3e4039fa
inv4fee2020 <inv4fee2020@gmail.com>
2022-04-13 23:53:11 +0100

    db dr check

--- _plinode_restore.sh
+++ _plinode_restore.sh
@@ -255,16 +255,16 @@ FUNC_DB_DR_CHECK(){
             echo -e "${GREEN}##  this includes where you have reset your previous VPS installation to start again..${NC}"
             echo -e "${GREEN}##${NC}"
             echo
-            read -t15 -r -p "Are you performing a Full Restore to BLANK / NEW VPS ? (Y/n) " _RES_INPUT
+            read -t15 -r -p "Are you performing a Full Restore to BLANK / NEW VPS? - Please answer (Y)es or (N)o " _RES_INPUT
             if [ $? -gt 128 ]; then
                 #clear
                 echo
                 echo
                 echo "....timed out waiting for user response - proceeding as standard in-place restore to existing system..."
                 echo
-                DR_RESTORE=false
-                break
-                #FUNC_RESTORE_MENU;
+                #DR_RESTORE=false
+                #break
+                FUNC_RESTORE_MENU;
             fi
             case $_RES_INPUT in
                 [Yy][Ee][Ss]|[Yy]* ) 
