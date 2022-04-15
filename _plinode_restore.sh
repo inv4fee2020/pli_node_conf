ab71fba491908cfc2f369c868f60a699e0e5f650
inv4fee2020 <inv4fee2020@gmail.com>
2022-04-16 00:21:11 +0100

    debug

--- _plinode_restore.sh
+++ _plinode_restore.sh
@@ -54,9 +54,9 @@ FUNC_RESTORE_DECRYPT(){
     fi
 
 
-    #if [ -e ~/"plinode_$(hostname -f)".vars ]; then
+    if [ -e ~/"plinode_$(hostname -f)".vars ]; then
     source ~/"plinode_$(hostname -f)".vars
-    #fi
+    fi
 
 
     #BACKUP_FILE="$IFS"
